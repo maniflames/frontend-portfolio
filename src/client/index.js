c7d9b8eb545a829d18967ba68ad5b3ac9f78e4c8
maniflames <imani.dap@gmail.com>
2017-12-16 21:04:39 +0100

    keep build folder only

--- src/client/index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./index.prod'); // eslint-disable-line global-require
-} else {
-  module.exports = require('./index.dev'); // eslint-disable-line global-require
-}
