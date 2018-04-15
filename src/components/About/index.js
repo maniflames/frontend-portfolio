db3545318328e24280ad8e193782f9757f854a97
Imani Dap <maniflames@MBP-van-Imani.fritz.box>
2018-04-15 20:09:39 +0200

    highlighting parts in about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,16 +16,16 @@ function About() {
       <h1>Hi, I&#39;m Imani</h1>
         <p>
           Right now I&#39;m doing the bachelor Creative Media and Game Technologies in the Netherlands.
-          I&#39;m looking for an internship between September 2018 and February 2019, preferably abroad so I can experience something new.
+          I&#39;m looking for an <span>internship</span> between September 2018 and February 2019, preferably abroad so I can experience something new.
         </p>
         <p>
-          As a creative full-stack developer I get involved in a project right from the start.
-          Concepting, researching and communicating with clients is part of what I do.
+          As a <span>creative full-stack developer</span> I get involved in a project right from the start.
+           <span> Concepting, researching and communicating with clients</span> is part of what I do.
         </p>
         <p>
           In the 1,5 year that I&#39;ve been developing, I&#39;ve discovered that I love things that work.
-          It&#39;s probably the main reason why the back-end caught my attention first.
-          I&#39;ve developed more interests along the way and started playing around with three.js, Arduino & Raspberry Pi.
+          It&#39;s probably the main reason why the <span>back-end</span> caught my attention first.
+          I&#39;ve developed more interests along the way and started playing around with <span>three.js, Arduino & Raspberry Pi</span>.
           To keep it short, I believe that 3D and hardware can give someone an experience that can take over the imagination.
           Hopefully, there is a place where I can keep experimenting with these technologies or learn something else that will blow my mind.
         </p>
